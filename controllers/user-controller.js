86d05baa708bb7f5f1f07dc0cbcd98709fcf4fbc
janithr <janithr@xeptagon.com>
2022-06-29 22:15:54 +0530

    add basic login and signup

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,10 +1,28 @@
 const User = require('../model/User');
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken");
+const JWT_SECRET_KEY = "MyKey";
 
 const signup = async (req,res,next) => {
+    const {name , email ,password} = req.body;
+    
+    let checkUser; 
+
+    try {
+        checkUser = await User.findOne({email: email})
+    } catch (error) {
+        console.log(error)
+    }
+
+    if(checkUser) {
+        return res.status(400).json({message: "user already exists"})
+    }
+    
+    const hashedPassword = bcrypt.hashSync(password);
     const user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
+        name,
+        email,
+        password : hashedPassword
     })
 
     try {
@@ -16,4 +34,29 @@ const signup = async (req,res,next) => {
     return res.status(201).json({message:user})
 }
 
-exports.signup = signup
+const login = async (req,res,next) => {
+    const {email ,password} = req.body;
+    
+    let checkUser; 
+
+    try {
+        checkUser = await User.findOne({email: email})
+    } catch (error) {
+        console.log(error)
+    }
+
+    if(!checkUser) {
+        return res.status(400).json({message: "user not Found"})
+    }
+
+    const isPasswordCorrect = bcrypt.compareSync(password, checkUser.password);
+    
+    if(!isPasswordCorrect) {
+        return res.status(400).json({message: "Invalid Email/ Password"}); 
+    }
+
+    return res.status(200).json({message: "Successfully Logged In"});
+}
+
+exports.signup = signup;
+exports.login = login;
