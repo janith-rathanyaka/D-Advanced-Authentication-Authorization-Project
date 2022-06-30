75ba3896cb197183a99542007c6e09f6c0c5d8d2
janithr <janithr@xeptagon.com>
2022-06-30 22:25:00 +0530

    add files

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,11 @@
 const express  = require("express");
 const mongoose = require("mongoose");
+const cookieParser = require('cookie-parser')
 
 const router = require("./routes/user-routes");
 
 const app = express();
+app.use(cookieParser());
 app.use(express.json());
 app.use("/api", router);
 
--- Backend/controllers/user-controller.js
+++ Backend/controllers/user-controller.js
@@ -55,8 +55,57 @@ const login = async (req,res,next) => {
         return res.status(400).json({message: "Invalid Email/ Password"}); 
     }
 
-    return res.status(200).json({message: "Successfully Logged In"});
+    const token = jwt.sign({id:checkUser._id}, JWT_SECRET_KEY, {
+         expiresIn:"1hr"
+    });
+    
+    res.cookie(String(checkUser._id), token, {
+        path: '/',
+        expires: new Date(Date.now() + 1000 * 30), //30 seconds
+        httpOnly: true,
+        sameSite: 'lax'
+    } )
+
+    return res.status(200).json({message: "Successfully Logged In",
+        user: checkUser,token 
+    });
+}
+
+const verifyToken = (req,res,next) => {
+     const cookies = req.headers.cookie;
+     console.log(cookies)
+     const token = cookies.split("=")[1];
+    if(!token) {
+        res.status(404).json({message: "No token found!"})
+    }
+
+    jwt.verify(String(token), JWT_SECRET_KEY , (err, user) => {
+        if(err) {
+            return res.status(400).json({message:"Invalid Token"})
+        }
+        console.log(user.id)
+        req.id = user.id;
+    });
+   next();
+}
+
+const getUser = async (req,res,next) => {
+   const userId = req.id;
+   let user;
+   try {
+       user = await User.findById(userId, "-password");
+   } catch (error) {
+       return new Error(error)
+   }
+
+   if(!user) {
+       return res.status(404).json({message: "User Not Found"});
+   }
+
+   return res.status(200).json({user});
 }
 
 exports.signup = signup;
 exports.login = login;
+exports.verifyToken = verifyToken;
+exports.getUser = getUser
